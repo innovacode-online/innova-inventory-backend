1d4081d5415e5c2f3ae6e42f2b349138a69e471c
Mattias <duartemattias3@gmail.com>
2023-06-08 22:19:34 -0400

    endpoints and category seeder

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// PRODUCTS ENDPOINTS
+//? MENCIONAR CONVENCIONES DE NOMBRE DE FUNCIONES
+Route::get('/products',         [ ProductController::class, 'index'     ]);
+Route::get('/products/{id}',    [ ProductController::class, 'show'      ]);
+Route::post('/products',        [ ProductController::class, 'store'     ]);
+Route::patch('/products/{id}',  [ ProductController::class, 'update'    ]);
+Route::delete('/products/{id}', [ ProductController::class, 'destroy'   ]);
+
+
+// CATEGORIES ENDPOINTS
+Route::get('/categories',         [ CategoryController::class, 'index'     ]);
+Route::get('/categories/{id}',    [ CategoryController::class, 'show'      ]);
+Route::post('/categories',        [ CategoryController::class, 'store'     ]);
+Route::patch('/categories/{id}',  [ CategoryController::class, 'update'    ]);
+Route::delete('/categories/{id}', [ CategoryController::class, 'destroy'   ]);
